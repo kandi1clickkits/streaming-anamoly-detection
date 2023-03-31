cee2fe0b58975f187ceb538ca40593d585b8e5d2
kandi1clickkits <113971260+kandi1clickkits@users.noreply.github.com>
2023-03-31 17:54:26 +0530

    Files added

---
+++ kafkaconsumer.py
@@ -0,0 +1,37 @@
+from kafka import KafkaConsumer, KafkaProducer
+import json
+import pickle
+from json import loads
+import numpy as np
+import sys
+import time
+import os
+
+model_path = os.path.abspath(r"model/finalized_model.pickle")
+
+with open(model_path, 'rb') as f:
+    clf = pickle.load(f)
+
+bootstrap_servers = ['localhost:9092']
+topicname = 'potential-drowning'
+
+consumer = KafkaConsumer(topicname, bootstrap_servers=bootstrap_servers, auto_offset_reset = 'latest', value_deserializer= lambda x: loads(x.decode('utf-8')))
+
+try:
+    for message in consumer:
+        time.sleep(1)
+        record = json.loads(json.dumps(message.value))
+        
+        data = list(record.values())        
+        prediction = clf.predict([data])        
+        if prediction[0] == -1:
+            record['potentialDrowning'] = 1
+        else: record['potentialDrowning'] = 0
+        
+        score = -1*clf.score_samples([data])
+        record["score"] = np.round(score[0], 3)
+        print(record)
+    consumer.close()
+
+except KeyboardInterrupt:
+    sys.exit()
---
+++ kafkaproducer.py
@@ -0,0 +1,24 @@
+from kafka import KafkaConsumer, KafkaProducer
+import json
+import time
+from json import loads
+import csv
+
+bootstrap_servers = ['localhost:9092']
+topicname = 'potential-drowning'
+producer = KafkaProducer(bootstrap_servers = bootstrap_servers)
+producer = KafkaProducer()
+
+csvfile = open('test.csv', 'r')
+fieldnames = ("HR","SpO2")
+reader = csv.DictReader(csvfile, fieldnames)
+header = next(reader)
+if header != None:
+    for row in reader:
+        print(row)
+        print(json.dumps(row))
+        ack = producer.send(topicname, json.dumps(row).encode('utf-8'))
+        time.sleep(1)
+        metadata = ack.get()
+        print(row)
+        print("Data stream sent to topic and partition: " + metadata.topic, metadata.partition)
